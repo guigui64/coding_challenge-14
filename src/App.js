c5189183e4918df76b6b4f0442d5ecdcad82cde2
guigui64 <guillaume.comte10@gmail.com>
2019-02-17 22:48:58 +0100

    Init react app + emojis

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './components/Card';
+import './App.css';
+
+const App = () => {
+    let emojis = '🐶 🐱 🐭 🐹 🐰 🦊 🐻 🐼 🐨 🐯 🦁 🐮 🐷 🐽 🐸 🐵 🙈 🙉 🙊 🐒 🐔 🐧 🐦 🐤 🐣 🐥 🦆 🦅 🦉 🦇 🐺 🐗 🐴 🦄 🐝 🐛 🦋 🐌 🐚 🐞 🐜 🦗 🕷 🕸 🦂 🐢 🐍 🦎 🦖 🦕 🐙 🦑 🦐 🦀 🐡 🐠 🐟 🐬 🐳 🐋 🦈 🐊 🐅 🐆 🦓 🦍 🐘 🦏 🐪 🐫 🦒 🐃 🐂 🐄 🐎 🐖 🐏 🐑 🐐 🦌 🐕 🐩 🐈 🐓 🦃 🕊 🐇 🐁 🐀 🐿 🦔 🐾 🐉 🐲 🌵 🎄 🌲 🌳 🌴 🌱 🌿 ☘️ 🍀 🎍 🎋 🍃 🍂 🍁 🍄 🌾 💐 🌷 🌹 🥀 🌺 🌸 🌼 🌻 🌞 🌝 🌛 🌜 🌚 🌕 🌖 🌗 🌘 🌑 🌒 🌓 🌔 🌙 🌎 🌍 🌏 💫 ⭐️ 🌟 ✨ ⚡️ ☄️ 💥 🔥 🌪 🌈 ☀️ 🌤 ⛅️ 🌥 ☁️ 🌦 🌧 ⛈ 🌩 🌨 ❄️ ☃️ ⛄️ 🌬 💨 💧 💦 ☔️ ☂️ 🌊 🌫'.split(' ');
+    let selectedEmojis = [];
+    for (let i = 0; i < 18; i++) {
+        let index = Math.floor(Math.random() * emojis.length);
+        selectedEmojis.push(emojis.splice(index, 1));
+    }
+    return (
+        <div>
+            {
+                selectedEmojis.map((emoji, i) => <Card emoji={emoji} key={i} />)
+            }
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Card = ({emoji}) => {
+    return <div>{emoji}</div>
+}
+
+export default Card;
